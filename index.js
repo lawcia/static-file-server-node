3ed7871dd1d16fe2e5774114954d097a8440e88d
lawcia <lawrencianjume@gmail.com>
2021-05-26 20:59:52 +0100

    update node server to serve files from url path

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const http = require('http');
-const port = process.env.PORT || 3000;
+const fs = require('fs');
+const port = process.env.PORT || 8080;
 
-http.createServer(function(request, response) {
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('Hello World!');
+http.createServer(function (request, response) {
+  if(!/^\/(public|favicon.ico$)/.test(request.url)){
+    response.writeHead(400);
+    response.end(JSON.stringify({message: 'not authorised'}));
+    return;
+  }
+  fs.readFile(__dirname + request.url, function (err, data) {
+    if (err) {
+      response.writeHead(404);
+      response.end(JSON.stringify(err));
+      return;
+    }
+    const path = request.url.split(".");
+    const extension = path[path.length - 1];
+    const contentTypes = {
+      html: "text/html",
+      css: "text/css",
+      js: "text/javascript"
+    };
+    const headers = {
+      "Content-Type": contentTypes[extension]
+    };
+    response.writeHead(200, headers);
+    response.end(data);
+  });
 }).listen(port);
 
 console.log(`Server running at http://localhost:${port}`);
